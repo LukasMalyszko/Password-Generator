aed9e293d765e21075f83ff067410c369aacf3e7
Lukasz Malyszko <lukas.malyszko@gmail.com>
2023-06-06 12:12:53 +0200

    fix random space

--- src/modules/HomeModule/components/PasswordGenerator/PasswordGenerator.tsx
+++ src/modules/HomeModule/components/PasswordGenerator/PasswordGenerator.tsx
@@ -12,10 +12,9 @@ export const PasswordGenerator: React.FC = () => {
     includeLowercase: false,
     includeNumbers: true,
     includeSymbols: true,
-    generatedPassword: " ",
+    generatedPassword: "",
     isCopied: false,
   });
-
 
   const passwordStrength = calculatePasswordStrength();
 
@@ -46,16 +45,11 @@ export const PasswordGenerator: React.FC = () => {
   const handleClickCopy = () => {
     const textField = document.createElement("textarea");
     textField.value = state.generatedPassword;
-    document.body.appendChild(textField);
-    textField.select();
-    document.execCommand("copy");
-    textField.remove();
-
+    navigator.clipboard.writeText(textField.value);
     setState((prevState) => ({
       ...prevState,
       isCopied: true,
     }));
-
     setTimeout(() => {
       setState((prevState) => ({
         ...prevState,
@@ -65,12 +59,12 @@ export const PasswordGenerator: React.FC = () => {
   };
 
   const generatePassword = (passwordLenght: number) => {
-    let charset = " ";
+    let charset = "_"; 
     if (state.includeUppercase) charset += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     if (state.includeLowercase) charset += "abcdefghijklmnopqrstuvwxyz";
     if (state.includeNumbers) charset += "0123456789";
     if (state.includeSymbols) charset += "!@#$%^&*()_+~|}{[]:;?><,./-=";
-
+    
     let generatedPassword = "";
     for (let i = 0; i < passwordLenght; i++) {
       const randomIndex = Math.floor(Math.random() * charset.length);
@@ -79,6 +73,7 @@ export const PasswordGenerator: React.FC = () => {
     setState((prevState) => ({
       ...prevState,
       generatedPassword: generatedPassword,
+      
     }));
   };
 
@@ -87,11 +82,16 @@ export const PasswordGenerator: React.FC = () => {
     setState((prevState) => ({
       ...prevState,
       [name]: checked,
-    }))
+    }));
   };
 
   function calculatePasswordStrength() {
-    const { includeUppercase, includeLowercase, includeNumbers, includeSymbols } = state;
+    const {
+      includeUppercase,
+      includeLowercase,
+      includeNumbers,
+      includeSymbols,
+    } = state;
     const strengthFactors = [
       includeUppercase,
       includeLowercase,
@@ -99,154 +99,177 @@ export const PasswordGenerator: React.FC = () => {
       includeSymbols,
     ];
     const strength = strengthFactors.filter((factor) => factor).length;
-    const lengthStrength = Math.floor((state.progress + minPasswordLength) / 12);
+    const lengthStrength = Math.floor(
+      (state.progress + minPasswordLength) / 12
+    );
     return strength + lengthStrength;
   }
 
-  const barColor =
-    passwordStrength <= 2
-      ? "#FF3A3A"
-      : passwordStrength > 3
-      ? "#30BF19"
-      : passwordStrength > 2
-      ? "#FFC225"
-      : "";
-
   return (
-    <>
-      <div className="pass-generator-component">
-        <div className="pass-generator-component__header">
-          Password Generator
-        </div>
-        <div className="pass-generator-component__length-container">
-          <div className="pass-generator-component__container-label">
-            <div className="pass-generator-component__container-text">
-              Character Lenght
-            </div>
-            <div className="pass-generator-component__length-number">
-              {state.progress + minPasswordLength}
-            </div>
+    <div className="pass-generator-component">
+      <div className="pass-generator-component__header">Password Generator</div>
+      <div className="pass-generator-component__length-container">
+        <div className="pass-generator-component__container-label">
+          <div className="pass-generator-component__container-text">
+            Character Lenght
           </div>
-          <div className="pass-generator-component__length-range">
-            <RangeInput
-              value={state.progress}
-              min={0}
-              max={16 - minPasswordLength}
-              onChange={handleProgressChange}
-            />
+          <div className="pass-generator-component__length-number">
+            {state.progress + minPasswordLength}
           </div>
         </div>
-        <div className="pass-generator-component__radio-container">
-          <div className="pass-generator-component__radio-section">
-            <div className="pass-generator-component__choose-container">
-              <input
-                className="pass-generator-component__checkbox"
-                type="checkbox"
-                name="includeUppercase"
-                checked={state.includeUppercase}
-                onChange={handleCheckboxChange}
-              />
-              <div className="pass-generator-component__choose-label">
-                Include Uppercase Letter
-              </div>
-            </div>
-            <div className="pass-generator-component__choose-container">
-              <input
-                className="pass-generator-component__checkbox"
-                type="checkbox"
-                name="includeLowercase"
-                checked={state.includeLowercase}
-                onChange={handleCheckboxChange}
-              />
-              <div className="pass-generator-component__choose-label">
-                Include Lowercase Letter
-              </div>
-            </div>
-          </div>
-          <div className="pass-generator-component__radio-section">
-            <div className="pass-generator-component__choose-container">
-              <input
-                className="pass-generator-component__checkbox"
-                type="checkbox"
-                name="includeNumbers"
-                checked={state.includeNumbers}
-                onChange={handleCheckboxChange}
-              />
-              <div className="pass-generator-component__choose-label">
-                Include Numbers
-              </div>
-            </div>
-            <div className="pass-generator-component__choose-container">
-              <input
-                className="pass-generator-component__checkbox"
-                type="checkbox"
-                name="includeSymbols"
-                checked={state.includeSymbols}
-                onChange={handleCheckboxChange}
-              />
-              <div className="pass-generator-component__choose-label">
-                Include Symbols
-              </div>
-            </div>
-          </div>
+        <div className="pass-generator-component__length-range">
+          <RangeInput
+            value={state.progress}
+            min={0}
+            max={16 - minPasswordLength}
+            onChange={handleProgressChange}
+          />
         </div>
-        <div className="pass-generator-component__strenght-container">
-          <div className="pass-generator-component__container-label">
-            <div className="pass-generator-component__container-text">
-              Strenght
-            </div>
-            <div className="pass-generator-component__strenght-value">
-              {passwordStrength <= 2 && "Low password strength"}
-              {passwordStrength === 3 && "Medium password strength"}
-              {passwordStrength >= 4 && "High password strength"}
-            </div>
-          </div>
-          <div className="pass-generator-component__strenght-bars">
-            <div
-              style={{ "--color-on-bar": barColor } as React.CSSProperties}
-              className={`bar ${passwordStrength >= 0 ? "active" : ""}`}
-            ></div>
-            <div
-              style={{ "--color-on-bar": barColor } as React.CSSProperties}
-              className={`bar ${passwordStrength > 1 ? "active" : ""}`}
-            ></div>
-            <div
-              style={{ "--color-on-bar": barColor } as React.CSSProperties}
-              className={`bar ${passwordStrength > 2 ? "active" : ""}`}
-            ></div>
-            <div
-              style={{ "--color-on-bar": barColor } as React.CSSProperties}
-              className={`bar ${passwordStrength > 3 ? "active" : ""}`}
-            ></div>
-          </div>
-        </div>
-        <div className="pass-generator-component__output">
-          <div className="pass-generator-component__output-value">
-            {state.generatedPassword}
-          </div>
-          <div
-            className="image-container"
-            onClick={() => generatePassword(state.progress + minPasswordLength)}
-          >
-            <img src="/refresh.svg" alt="refresh icon" />
-          </div>
-        </div>
-        <button
-          className="pass-generator-component__button"
-          onClick={handleClickCopy}
-        >
-          {state.isCopied ? (
-            <div className="text">Copied!</div>
-          ) : (
-            <>
-              <div className="image-container">
-                <img src="/copy-img.svg" alt="copy icon" />
-              </div>
-              <div className="text">Copy Password</div>
-            </>
-          )}
-        </button>
       </div>
-    </>
+      <div className="pass-generator-component__radio-container">
+        <div className="pass-generator-component__radio-section">
+          <div className="pass-generator-component__choose-container">
+            <input
+              id="uppercase"
+              className="pass-generator-component__checkbox"
+              type="checkbox"
+              name="includeUppercase"
+              checked={state.includeUppercase}
+              onChange={handleCheckboxChange}
+            />
+            <label
+              htmlFor="uppercase"
+              className="pass-generator-component__choose-label"
+            >
+              Include Uppercase Letter
+            </label>
+          </div>
+          <div className="pass-generator-component__choose-container">
+            <input
+              id="lowercase"
+              className="pass-generator-component__checkbox"
+              type="checkbox"
+              name="includeLowercase"
+              checked={state.includeLowercase}
+              onChange={handleCheckboxChange}
+            />
+            <label
+              htmlFor="lowercase"
+              className="pass-generator-component__choose-label"
+            >
+              Include Lowercase Letter
+            </label>
+          </div>
+        </div>
+        <div className="pass-generator-component__radio-section">
+          <div className="pass-generator-component__choose-container">
+            <input
+              id="numbers"
+              className="pass-generator-component__checkbox"
+              type="checkbox"
+              name="includeNumbers"
+              checked={state.includeNumbers}
+              onChange={handleCheckboxChange}
+            />
+            <label
+              htmlFor="numbers"
+              className="pass-generator-component__choose-label"
+            >
+              Include Numbers
+            </label>
+          </div>
+          <div className="pass-generator-component__choose-container">
+            <input
+              id="symbols"
+              className="pass-generator-component__checkbox"
+              type="checkbox"
+              name="includeSymbols"
+              checked={state.includeSymbols}
+              onChange={handleCheckboxChange}
+            />
+            <label
+              htmlFor="symbols"
+              className="pass-generator-component__choose-label"
+            >
+              Include Symbols
+            </label>
+          </div>
+        </div>
+      </div>
+      <div className="pass-generator-component__strenght-container">
+        <div className="pass-generator-component__container-label">
+          <div className="pass-generator-component__container-text">
+            Strenght
+          </div>
+          <div className="pass-generator-component__strenght-value">
+            {passwordStrength <= 2 && "Low password strength"}
+            {passwordStrength === 3 && "Medium password strength"}
+            {passwordStrength >= 4 && "High password strength"}
+          </div>
+        </div>
+        <div className="pass-generator-component__strenght-bars">
+          <div
+            className={`bar ${
+              passwordStrength > 3
+                ? "green-bar"
+                : passwordStrength > 2
+                ? "yellow-bar"
+                : passwordStrength <= 2
+                ? "red-bar"
+                : ""
+            }`}
+          ></div>
+          <div
+            className={`bar ${
+              passwordStrength > 3
+                ? "green-bar"
+                : passwordStrength > 2
+                ? "yellow-bar"
+                : passwordStrength === 2
+                ? "red-bar"
+                : ""
+            }`}
+          ></div>
+          <div
+            className={`bar ${
+              passwordStrength > 3
+                ? "green-bar"
+                : passwordStrength > 2
+                ? "yellow-bar"
+                : ""
+            }`}
+          ></div>
+          <div
+            className={`bar ${passwordStrength > 3 ? "green-bar" : ""}`}
+          ></div>
+        </div>
+      </div>
+      <div className="pass-generator-component__output">
+        <div className="pass-generator-component__output-value">
+          {state.generatedPassword}
+        </div>
+        <div
+          className="image-container"
+          onClick={() => generatePassword(state.progress + minPasswordLength)}
+        >
+          <img src="/refresh.svg" alt="refresh icon" />
+        </div>
+      </div>
+      <button
+        className="pass-generator-component__button"
+        onClick={handleClickCopy}
+      >
+        {state.isCopied ? (
+          <div className="text">Copied!</div>
+        ) : (
+          <>
+            <div className="image-container">
+              <img src="/copy-img.svg" alt="copy icon" />
+            </div>
+            <div className="text">Copy Password</div>
+          </>
+        )}
+      </button>
+    </div>
   );
 };
